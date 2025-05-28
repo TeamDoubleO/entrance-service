1b9db937fbd7d90e291f75b134c06cc9e89b680e
midday2612 <midday2612@gmail.com>
2025-05-28 14:38:49 +0900

    KW-503/chore: entrance -> entrances

--- src/main/java/com/doubleo/entranceservice/controller/EntranceController.java
+++ src/main/java/com/doubleo/entranceservice/controller/EntranceController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/entrance")
+@RequestMapping("/entrances")
 @RequiredArgsConstructor
 public class EntranceController {
     private final EntranceService entranceService;
